143d0f61f2dea4a31d7685386b6ce20a9decd937
NotTheEvilOne <git-NTEO@vplace.de>
2021-03-20 07:28:44 +0100

    Replace "__slots__" lists with tuples
    (Trivial)

--- src/dpt_mime_type/mime_type.py
+++ src/dpt_mime_type/mime_type.py
@@ -45,7 +45,7 @@ Provides mime type related methods on top of Python basic ones.
              Mozilla Public License, v. 2.0
     """
 
-    __slots__ = [ "__weakref__", "definitions", "extensions" ]
+    __slots__ = ( "__weakref__", "definitions", "extensions" )
     """
 python.org: __slots__ reserves space for the declared variables and prevents
 the automatic creation of __dict__ and __weakref__ for each instance.
